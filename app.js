cfb16aa55452b2f515a253e98532287d2ea3f782
Armands Leimanis <armands@gmail.com>
2021-07-29 20:17:48 +0300

    Axios

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const fetch = require("node-fetch");
+//const fetch = require("node-fetch");
+const axios = require('axios').default;
 const Parser = require('rss-parser');
 const parser = new Parser({
   customFields: {
@@ -20,21 +21,21 @@ const IR_RSS_URL = `https://ir.lv/feed/`;
 async function getNews() {
     //const apollo = await parser.parseURL(APOLLO_RSS_URL);
 
-    const apollo = await fetch(APOLLO_RSS_URL);
-    const tvnet = await fetch(TVNET_RSS_URL);
-    const delfi = await fetch(DELFI_RSS_URL);
-    const lsm = await fetch(LSM_RSS_URL);
+    const apollo = await axios.get(APOLLO_RSS_URL);
+    const tvnet = await axios.get(TVNET_RSS_URL);
+    const delfi = await axios.get(DELFI_RSS_URL);
+    const lsm = await axios.get(LSM_RSS_URL);
     // const ir = await parser.parseURL(IR_RSS_URL);
 
-    const apollo_text = await apollo.text();
-    const tvnet_text = await tvnet.text();
-    const delfi_text = await delfi.text();
-    const lsm_text = await lsm.text();
+    // const apollo_text = await apollo.text();
+    // const tvnet_text = await tvnet.text();
+    // const delfi_text = await delfi.text();
+    // const lsm_text = await lsm.text();
 
-    const apollo_result = await parser.parseString(apollo_text);
-    const tvnet_result = await parser.parseString(tvnet_text);
-    const delfi_result = await parser.parseString(delfi_text);
-    const lsm_result = await parser.parseString(lsm_text);
+    const apollo_result = await parser.parseString(apollo.data);
+    const tvnet_result = await parser.parseString(tvnet.data);
+    const delfi_result = await parser.parseString(delfi.data);
+    const lsm_result = await parser.parseString(lsm.data);
   
     return { apollo: apollo_result, tvnet: tvnet_result, delfi: delfi_result, lsm: lsm_result };
 }
