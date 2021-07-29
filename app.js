d215ef5fbd6951a646c9a90959b8705a8e276ced
Armands Leimanis <armands@gmail.com>
2021-07-29 13:56:29 +0300

    Added Delfi image support

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const fetch = require("node-fetch");
 const Parser = require('rss-parser');
-const parser = new Parser();
+const parser = new Parser({
+  customFields: {
+  item: ['media:content']
+  }
+});
 
 const app = express();
 app.use(cors());
@@ -11,14 +15,16 @@ const APOLLO_RSS_URL = `https://apollo.lv/rss`;
 const TVNET_RSS_URL = `https://tvnet.lv/rss`;
 const DELFI_RSS_URL = `https://delfi.lv/rss/?channel=delfi`;
 const LSM_RSS_URL = `https://lsm.lv/rss/`;
+const IR_RSS_URL = `https://ir.lv/feed/`;
 
 async function getNews() {
     const apollo = await parser.parseURL(APOLLO_RSS_URL);
     const tvnet = await parser.parseURL(TVNET_RSS_URL);
     const delfi = await parser.parseURL(DELFI_RSS_URL);
     const lsm = await parser.parseURL(LSM_RSS_URL);
-    
-    return { apollo, tvnet, delfi, lsm };
+    // const ir = await parser.parseURL(IR_RSS_URL);
+
+    return { apollo, tvnet, delfi, lsm};
 }
 
 app.get("/", async (req, res) => {
