3157b8caf6a0d2a53e9a32fddc81d3db7b4aca9b
Armands Leimanis <armands@gmail.com>
2021-07-29 22:01:41 +0300

    try / catch

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const Parser = require('rss-parser');
 const parser = new Parser({
+  timeout: 5000,
   customFields: {
   item: ['media:content']
   }
@@ -14,19 +15,40 @@ const APOLLO_RSS_URL = `https://apollo.lv/rss`;
 const TVNET_RSS_URL = `https://tvnet.lv/rss`;
 const DELFI_RSS_URL = `https://delfi.lv/rss/?channel=delfi`;
 const LSM_RSS_URL = `https://lsm.lv/rss/`;
-const IR_RSS_URL = `https://ir.lv/feed/`;
 
 async function getNews() {
-    const apollo = await parser.parseURL(APOLLO_RSS_URL);
-    const tvnet = await parser.parseURL(TVNET_RSS_URL);
-    const delfi = await parser.parseURL(DELFI_RSS_URL);
-    const lsm = await parser.parseURL(LSM_RSS_URL);
-    // const ir = await parser.parseURL(IR_RSS_URL);
+    let apollo, tvnet, delfi, lsm;
 
+    try {
+      apollo = await parser.parseURL(APOLLO_RSS_URL);
+    } catch (e) { 
+      console.log(`apollo:`, e);
+    }
+
+    try {
+      tvnet = await parser.parseURL(TVNET_RSS_URL);
+    } catch (e) { 
+      console.log(`tvnet`, e);
+    }
+
+    try {
+      delfi = await parser.parseURL(DELFI_RSS_URL);
+    } catch (e) { 
+      console.log(`delfi`, e);
+    }
+
+    try {
+      lsm = await parser.parseURL(LSM_RSS_URL);
+    } catch (e) { 
+      console.log(`lsm`, e);
+    }
+    
     return { apollo, tvnet, delfi, lsm};
 }
 
 app.get("/", async (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  console.log('Request from', ip);
   const news = await getNews();
   res.send(news);
 });
