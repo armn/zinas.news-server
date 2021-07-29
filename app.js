e924da58bf3dcabebf4d1ac41939b4c65bbcbd14
Armands Leimanis <armands@gmail.com>
2021-07-29 19:55:30 +0300

    Node fetch instead or parser (to work on DigitalOcean)

--- app.js
+++ app.js
@@ -14,17 +14,29 @@ app.use(cors());
 const APOLLO_RSS_URL = `https://apollo.lv/rss`;
 const TVNET_RSS_URL = `https://tvnet.lv/rss`;
 const DELFI_RSS_URL = `https://delfi.lv/rss/?channel=delfi`;
-const LSM_RSS_URL = `https://lsm.lv/rss/`;
+const LSM_RSS_URL = `https://www.lsm.lv/rss`;
 const IR_RSS_URL = `https://ir.lv/feed/`;
 
 async function getNews() {
-    const apollo = await parser.parseURL(APOLLO_RSS_URL);
-    const tvnet = await parser.parseURL(TVNET_RSS_URL);
-    const delfi = await parser.parseURL(DELFI_RSS_URL);
-    const lsm = await parser.parseURL(LSM_RSS_URL);
+    //const apollo = await parser.parseURL(APOLLO_RSS_URL);
+
+    const apollo = await fetch(APOLLO_RSS_URL);
+    const tvnet = await fetch(TVNET_RSS_URL);
+    const delfi = await fetch(DELFI_RSS_URL);
+    const lsm = await fetch(LSM_RSS_URL);
     // const ir = await parser.parseURL(IR_RSS_URL);
 
-    return { apollo, tvnet, delfi, lsm};
+    const apollo_text = await apollo.text();
+    const tvnet_text = await tvnet.text();
+    const delfi_text = await delfi.text();
+    const lsm_text = await lsm.text();
+
+    const apollo_result = await parser.parseString(apollo_text);
+    const tvnet_result = await parser.parseString(tvnet_text);
+    const delfi_result = await parser.parseString(delfi_text);
+    const lsm_result = await parser.parseString(lsm_text);
+  
+    return { apollo: apollo_result, tvnet: tvnet_result, delfi: delfi_result, lsm: lsm_result };
 }
 
 app.get("/", async (req, res) => {
